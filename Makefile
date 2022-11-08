f4325e0cc7775d0bfd5d6f9529231529c555c53c
EnigmaK9 <enigmak9@protonmail.com>
2022-11-08 04:10:10 -0600

    feat: added exercise 4 script

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-CLFLAGS=-Wall -
+CLFLAGS=-Wall -g
 
 clean:
 	rm -f ex1
+	rm -f ex2
+	rm -f ex3
--- ex3.c
+++ ex3.c
@@ -4,8 +4,9 @@ int main()
     int age = 10;
     int height = 72;
 
-    printf("I am %d years old. \n", age);
-    printf("I am %d inches tall. \n", height);
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
 
     return 0;
 }
+
